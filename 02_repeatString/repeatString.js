5eb61dca6bb77b120f8ee9a992f7805369ca6554
Ayman <aymanelkarroussi@gmail.com>
2024-09-14 06:54:29 +0100

    repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+let res="";
+for(let i=num;i>0;i--){
+res+=string;
+};
+return res;
 };
 
 // Do not edit below this line
