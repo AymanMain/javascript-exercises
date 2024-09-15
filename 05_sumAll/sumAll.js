eb436151b619096bd87216b4d3652494b0cbe323
Ayman <aymanelkarroussi@gmail.com>
2024-09-15 14:40:04 +0100

    sumAll 7/7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(first,last) {
+/*sumAll(first,last) check if positive*/if ((first<0||last<0)||(!Number.isInteger(first))||(!Number.isInteger(last))){return "ERROR"};
+/* if positive , for i(first->laster)=>res+=i} */
+if (first>last){let temp=first;first=last;last=temp};
+let res=0;
+for(let i=first;i<=last;i++){
+    res+=i;
+};
+return res;
 };
 
 // Do not edit below this line
