d2b2e72a891ad8f556d2183140eb865008fff1fd
Ayman <aymanelkarroussi@gmail.com>
2024-09-15 19:14:57 +0100

    fibonacci 11/11

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
 const palindromes = function (str) {
-let string=Array.from(str.toLowerCase().replace(/[^a-z0-9]/g, ''));
+let string=Array.from(str.toLowerCase());
 for(let i=0;i<(string.length/2);i++){
     if(string[i]!==string[(string.length)-1-i]){return false;};
 };
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(numS) {
+    let num=parseInt(numS);
+    if(num<0){return "OOPS";}
+    else if(num==0){return 0;}
+    else if(num==1){return 1;};
+    
+    let fibo=0;
+    let n_2=0;
+    let n_1=1;
 
+        for(let i=2;i<=num;i++){
+            fibo=n_2+n_1;
+            n_2=n_1;
+            n_1=fibo;
+        };
+    return fibo;
 };
-
+/* 
+num-2=
+num-1=
+fibo=num-2 + num-1*/
 // Do not edit below this line
 module.exports = fibonacci;
