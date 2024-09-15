a1725b5b2c0467cd573b11527793633ca587ec7f
Ayman <aymanelkarroussi@gmail.com>
2024-09-15 12:43:01 +0100

    removeFromArray 8/8

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
 const removeFromArray = function() {
+    let args=Array.from(arguments);
+    let array=args[0];
+    let elements=args.toSpliced(0,1);
+    const size=array.length;
+
+    for(let j=0;j<elements.length;j++){
+        for (let i=0;i<size;++i){
+            if(array[i]===(elements[j])){
+                while(array[i]===(elements[j])){
+                    array.splice(i,1);
+                };
+            };   
+        };
+    };
+    return array;
 };
 
 // Do not edit below this line
