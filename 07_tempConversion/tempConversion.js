65907e3b4c62ce8d7ff90848d4eb26460e160c29
Ayman <aymanelkarroussi@gmail.com>
2024-09-15 15:58:55 +0100

    convertTemperature 6/6

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(cel) {
+  // f=(x-32)*(5/9)
+  return res=Math.round(((cel-32)*(5/9))*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(frn) {
+  // c=(x*(9/5)+32)
+  return res=Math.round(((frn*(9/5))+32)*10)/10;
 };
 
 // Do not edit below this line
