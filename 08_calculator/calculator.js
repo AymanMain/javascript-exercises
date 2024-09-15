a8af0df5c4c748963830c4181382f7e390d326d3
Ayman <aymanelkarroussi@gmail.com>
2024-09-15 16:27:59 +0100

    calculator 16/16

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
+const add = function(add1,add2) {
+	return res=add1+add2;
+};
+
+const subtract = function(sub1,sub2) {
+  return res=sub1-sub2;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(tosum) {
+  let sumR=0
+  for(let i=0;i<tosum.length;i++){
+    sumR+=tosum[i];
+  };
+  return sumR;
 };
 
-const sum = function() {
-	
+const multiply = function(tomul) {
+  let mulR=1;
+for(let i=0;i<tomul.length;i++){
+  mulR*=tomul[i];};
+  return mulR;
 };
 
-const multiply = function() {
-
+const power = function(pow1,pow2) {
+	return Math.pow(pow1,pow2);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(fct) {
+	if (fct==0){return 1;};
+  let res=1;
+  for (let i=1;i<=fct;i++){
+    res*=i;};
+    return res;
 };
 
 // Do not edit below this line
