f15707e8445d76d08ae2d5e02d31f2bbae8ddd88
Ayman <aymanelkarroussi@gmail.com>
2024-09-15 09:40:28 +0100

    repeatString7/7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,11 @@
 const repeatString = function(string,num) {
 let res="";
+if(num<0){return "ERROR"}
+else{
 for(let i=num;i>0;i--){
 res+=string;
 };
-return res;
+return res;};
 };
 
 // Do not edit below this line
